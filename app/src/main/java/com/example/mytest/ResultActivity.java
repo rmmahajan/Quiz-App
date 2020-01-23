87f34b522dfb5a72941d975d0a141a91cb91db75
rmmahajan <manvi.mahajan20@gmail.com>
2020-01-23 18:58:14 +0530

    final app

---
+++ app/src/main/java/com/example/mytest/ResultActivity.java
@@ -0,0 +1,31 @@
+package com.example.mytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView t1,t2,t3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        t1 = findViewById(R.id.t6);
+        t2 = findViewById(R.id.t7);
+        t3 = findViewById(R.id.t8);
+
+        Intent i = getIntent();
+
+        String questions = i.getStringExtra("total");
+        String correct = i.getStringExtra("correct");
+        String wrong = i.getStringExtra("incorrect");
+
+        t1.setText(questions);
+        t2.setText(correct);
+        t3.setText(wrong);
+    }
+}
